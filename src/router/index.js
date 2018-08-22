c411381a966db03315451262a851a5bdc866f76e
steinls <3026235413@qq.com>
2018-08-22 14:15:13 +0800

    路由按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+let HelloWorld = () => import('../components/HelloWorld.vue')
 
 Vue.use(Router)
 
